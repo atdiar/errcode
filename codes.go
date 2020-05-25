4404aa9d4808511b339dd1d7529608c0ee99a0d8
atd <atdiar@gmail.com>
2020-05-25 12:44:53 +0200

    Errcode: Add unknown error code

--- codes.go
+++ codes.go
@@ -1,9 +1,10 @@
 // Package errcode defines common error codes used throughout libraries.
-// It is supposed to be extended. Creates a Domain SPecific Error Universe.
+// It is supposed to be extended. Creates a Domain Specific Error Universe.
 package errcode
 
 // The code names should be self-explanatory enough.
 const (
+	Unknown   = 0
 	BadCookie = iota + 127 // Don't ask why 127
 	BadSession
 	BadStorage
